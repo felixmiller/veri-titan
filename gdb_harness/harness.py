8e966bb265d395ecdcfb4ab36287f75e0057e681
Yi Zhou <yizhou7@outlook.com>
2020-06-26 12:34:08 -0400

    seems to be dumping register trace

--- gdb_harness/harness.py
+++ gdb_harness/harness.py
@@ -8,8 +8,8 @@ frame = gdb.selected_frame()
 block = frame.block()
 assert block.function is not None
 
-start = block.start
-end = block.end
+output_file = open("trace", "w+")
+start, end = block.start, block.end
 arch = frame.architecture()
 end_addr = end - 1
 
@@ -21,10 +21,12 @@ if not instruction['asm'].startswith('ret'):
         if instruction['asm'].startswith('ret'):
             end_addr = instruction['addr']
 
+
 while gdb.parse_and_eval("$pc") != end_addr:
     gdb.execute('ni', to_string=True)
     o = gdb.execute('i r', to_string=True)
-    # print(o)
+    output_file.write(o + "\n")
+output_file.close()
 
 gdb.execute('c')
 gdb.execute('quit')
